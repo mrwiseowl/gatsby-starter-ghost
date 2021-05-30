117e7b01f17cd92356cb43fe8aafbbdeca9a873f
David Meacham <david@mrwiseowl.com>
2021-05-29 17:49:21 -0700

    updated layout
    againn

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -31,7 +31,44 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                 <body className={bodyClass} />
             </Helmet>
 
-            <header class="globalNav overlayActive dropdownActive">
+            <div className="viewport">
+
+                <div className="viewport-top">
+                    {/* The main header section on top of the screen */}
+                    <header className="site-head" style={{ ...site.cover_image && { backgroundImage: `url(${site.cover_image})` } }}>
+                        <div className="container">
+                            <div className="site-mast">
+                                <div className="site-mast-left">
+                                    <Link to="/">
+                                        {site.logo ?
+                                            <img className="site-logo" src={site.logo} alt={site.title} />
+                                            : <Img fixed={data.file.childImageSharp.fixed} alt={site.title} />
+                                        }
+                                    </Link>
+                                </div>
+                                <div className="site-mast-right">
+                                    { site.twitter && <a href={ twitterUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/twitter.svg" alt="Twitter" /></a>}
+                                    { site.facebook && <a href={ facebookUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/facebook.svg" alt="Facebook" /></a>}
+                                    <a className="site-nav-item" href={ `https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/` } target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/rss.svg" alt="RSS Feed" /></a>
+                                </div>
+                            </div>
+                            { isHome ?
+                                <div className="site-banner">
+                                    <h1 className="site-banner-title">{site.title}</h1>
+                                    <p className="site-banner-desc">{site.description}</p>
+                                </div> :
+                                null}
+                            <nav className="site-nav">
+                                <div className="site-nav-left">
+                                    {/* The navigation items as setup in Ghost */}
+                                    <Navigation data={site.navigation} navClass="site-nav-item" />
+                                </div>
+                                <div className="site-nav-right">
+                                    <Link className="site-nav-button" to="/about">About</Link>
+                                </div>
+                            </nav>
+                        </div>
+                        <header class="globalNav overlayActive dropdownActive">
         <div class="container-lg">
             <ul class="navRoot">
                 <li class="navSection logo">
@@ -544,6 +581,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
             </div>
         </div>
     </header>
+                    </header>
 
                     <main className="site-main">
                         {/* All the main content gets inserted here, index.js, post.js */}
